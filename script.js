b5d7d7fac4677e1b0c0ad01bea55081d32283980
Miro1028 <mglanimous@gmail.com>
2023-08-02 21:29:40 +0800

    added optional city and farenheit and ceclius option

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
 // Fetch data from the API
 function fetchData() {
-    const apiKey = '0f9d512106c2dbe231e41f17e392bdaa'; // Replace with your API key
-    const city = 'Ulaanbaatar'; // Replace with the city you want to get the weather for
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    const subm = document.getElementById("subm");
+    subm.addEventListener('click', () => {
+        const apiKey = '0f9d512106c2dbe231e41f17e392bdaa'; // Replace with your API key
+        const city = document.getElementById("city-choosing").value; // Replace with the city you want to get the weather for
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => displayData(data))
+            .catch(error => console.error('Error fetching data:', error));
+    })
 
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => displayData(data))
-        .catch(error => console.error('Error fetching data:', error));
 }
 // Display data on the webpage
 function displayData(data) {
     const dataContainer = document.getElementById('data-container');
+    let celc = document.getElementById("celcius-radio");
+    let faren = document.getElementById("farenheit-radio");
+    let radios = document.getElementById("radios")
 
     if (data.main && data.main.temp) {
-        const temperature = (data.main.temp - 273.15).toFixed(2); // Convert Kelvin to Celsius and round to 2 decimal places
-        dataContainer.innerHTML = `<p>${Math.floor(temperature)}&#8451;</p>`;
+        console.log(celc)
+        if (celc.checked == true) {
+            let temperature = (data.main.temp - 273.15); // Convert Kelvin to Celsius and round to 2 decimal places
+            dataContainer.innerHTML = `<p>${Math.floor(temperature)}&#8451;</p>`;
+        } else if (faren.checked == true) {
+            let temperature = (data.main.temp); // Convert Kelvin to Celsius and round to 2 decimal places
+            dataContainer.innerHTML = `<p>${Math.floor(temperature)}&deg;F</p>`;
+        }
     } else {
         dataContainer.innerHTML = '<p>Temperature data not available.</p>';
     }
 }
+
 window.addEventListener('load', fetchData);
